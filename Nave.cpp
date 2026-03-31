943dd8df30a0473e694976303453c0bd950a5d46
Daniel Robledo <daniel.robledo13@cenyca.edu.mx>
2026-03-30 21:17:45 -0700

    Add files via upload

--- Nave.cpp
+++ Nave.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <conio.h>
 
-int main()
+void gotoxy(int x, int y)
 {
-	printf("*");
-
 	HANDLE hcon;
 	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD dwpos;
@@ -12,7 +11,30 @@ int main()
 	dwpos.Y = 5;
 
 	SetConsoleCursorPosition(hcon, dwpos);
-	printf("*");
+}
 
+int main()
+{
+	int x = 10, y = 10;
+	gotoxy(x, y); printf("*");
+
+	bool game_over = false;
+	while (!game_over)
+	{
+		if (_kbhit())
+		{
+			char tecla = _getch();
+			gotoxy(x, y);
+			printf(" ");
+			if (tecla == 'j') x--;
+			if (tecla == 'l') x++;
+			if (tecla == 'i') y--;
+			if (tecla == 'k') y++;
+			gotoxy(x, y);
+			printf("*");
+		}
+
+		Sleep(30);
+	}
 	return 0;
 }
