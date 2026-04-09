abd670b2dd5f0ed9b810a32b8e20a3b34c744dfa
Daniel Robledo <daniel.robledo13@cenyca.edu.mx>
2026-04-08 20:52:01 -0700

    Add files via upload

--- Nave.cpp
+++ Nave.cpp
@@ -1,40 +1,52 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <conio.h>
+#include <iostream>
+
+#define up 72
+#define left 75
+#define right 77
+#define down 80
 
 void gotoxy(int x, int y)
 {
 	HANDLE hcon;
 	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD dwpos;
-	dwpos.X = 4;
-	dwpos.Y = 5;
+	dwpos.X = x;
+	dwpos.Y = y;
 
 	SetConsoleCursorPosition(hcon, dwpos);
 }
 
+void OcultarCursor()
+{
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.dwSize = 50;
+	cci.bVisible = FALSE;
+
+	SetConsoleCursorInfo(hcon, &cci);
+}
+
 int main()
 {
+	OcultarCursor();
 	int x = 10, y = 10;
-	gotoxy(x, y); printf("*");
+	gotoxy(x,y); printf("*");
 
 	bool game_over = false;
 	while (!game_over)
 	{
-		if (_kbhit())
-		{
-			char tecla = _getch();
-			gotoxy(x, y);
-			printf(" ");
-			if (tecla == 'j') x--;
-			if (tecla == 'l') x++;
-			if (tecla == 'i') y--;
-			if (tecla == 'k') y++;
-			gotoxy(x, y);
-			printf("*");
-		}
+		char tecla = _getch();
+		gotoxy(x,y); printf(" ");
+		if (tecla == left) x--;
+		if (tecla == right) x++;
+		if (tecla == up) y--;
+		if (tecla == down) y++;
+		gotoxy(x,y); printf("*");
 
-		Sleep(30);
 	}
 	return 0;
 }
